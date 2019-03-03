f2d8b761bf3b0c68c5e0e1e6a4aaa1bba5971c88
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2019-03-03 16:01:55 +0300

    feat: print out weather forecast and current time for a single city.

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 'use strict';
 const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = 4000;
+const apiKey = '82ff927a912991c4d8f66fd6d3d3bd25';
 
+/**
+ *  @description to perform a request to openweathermap api
+ *  print out weather forecast and current time.
+ *  @param {Object[]} res - response object
+ *  @param {string} cityName - input to implement into request
+ */
+const getDataFromApi = (res, cityName) => {
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${
+    cityName}&appid=${apiKey}`;
+  request(url, (err, response, body) => {
+    console.log(err);
+    let dataPackage = JSON.parse(body);
+    if (dataPackage.cod === '404') {
+      let error = `Please re-check your input; ${dataPackage.message}`;
+      console.log('error:', error);
+      return res.json({
+        error,
+      });
+    } else {
+      console.log('body:', body);
+      let convertDate = new Date(dataPackage.dt * 1000).toISOString();
+      let detailedDescription = dataPackage.weather[0].description;
+      let currentTime = `Current Date and Time is ${convertDate} in ${
+        dataPackage.name
+      }.`;
+      let weatherForecast = `There is ${detailedDescription} and It is ${
+        dataPackage.main.temp
+      } degrees`;
+      console.log(weatherForecast);
+      res.json([currentTime, weatherForecast]);
+    }
+  });
+};
 /**
  *  @description listening the port.
  *  @param {number} port
@@ -11,6 +47,15 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`${port} listening`);
 });
+
+/**
+ *  @description using bodyParser to decode incoming data from post requests
+ */
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 /**
  *  @description serving index.html
@@ -35,5 +80,9 @@ app.use(express.static(__dirname + '/public'));
  *  @param {Object[]} req - request object
  *  @param {Object[]} res - response object
  */
+app.post('/result', (req, res) => {
+  const cityName = req.body.city;
+  getDataFromApi(res, cityName);
+});
 
 module.exports = app;
