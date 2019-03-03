8f63ff0101ca028a7622e572d8fb719a32567ef3
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2019-03-03 17:11:28 +0300

    feat: single file upload is implemented, results are printed to console.

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const async = require('async');
+const formidable = require('formidable');
+const fs = require('fs');
+const alert = require('alert-node');
 const app = express();
 
-const port = 4000;
+const port = 4001;
 const apiKey = '82ff927a912991c4d8f66fd6d3d3bd25';
 
 /**
@@ -14,8 +18,7 @@ const apiKey = '82ff927a912991c4d8f66fd6d3d3bd25';
  *  @param {string} cityName - input to implement into request
  */
 const getDataFromApi = (res, cityName) => {
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${
-    cityName}&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   request(url, (err, response, body) => {
     console.log(err);
     let dataPackage = JSON.parse(body);
@@ -40,6 +43,71 @@ const getDataFromApi = (res, cityName) => {
     }
   });
 };
+
+
+
+/**
+ *  @description to delete uploaded files from the uploads folder
+ *  If we have ten files in the folder, we are deleting all of them
+ *  while the user performing the eleventh upload
+ *  also we throw a warning when user performed the tenth upload
+ *  @param {Object[]} res - response object
+ *  @param {string} cityName - input to implement into request
+ */
+const deleteUploads = () => {
+  fs.readdir('./uploads', (err, files) => {
+    if (err) {
+      console.log(err);
+    }
+    if (files.length === 2) {
+      alert(
+        "Your uploaded files' history will be deleted after the next uploading perform!"
+      );
+    } else if (files.length === 3) {
+      files.map(file => {
+        try {
+          fs.unlinkSync(`./uploads/${file}`);
+        } catch (err) {
+          console.error(err);
+        }
+      });
+    }
+  });
+};
+
+/**
+ *  @description to perform a request to openweathermap api
+ *  print out weather forecast and current time.
+ *  @param {Object[]} item - expexted object from uploaded JSON file.
+ *  @param {function} sendData - callback function to pass to the async map.
+ */
+const getDataForBulkInputs = (item, sendData) => {
+  let weatherForecast;
+  let detailedDescription;
+  let convertDate;
+  console.log(typeof (item));
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${
+    item.locationName
+  }&appid=${apiKey}`;
+  request(url, function(err, response, body) {
+    let dataPackage = JSON.parse(body);
+    if (dataPackage.cod === '404') {
+      weatherForecast = `Please re-check your input; ${
+        dataPackage.message}`;
+    } else {
+      convertDate = new Date(dataPackage.dt * 1000).toISOString();
+      detailedDescription = dataPackage.weather[0].description;
+      weatherForecast = `Current Date and Time is ${convertDate} in ${
+        dataPackage.name
+      };There is ${detailedDescription} and It is ${
+        dataPackage.main.temp
+      } degrees`;
+    }
+    sendData(err, weatherForecast);
+  });
+};
+
+
 /**
  *  @description listening the port.
  *  @param {number} port
@@ -83,6 +151,46 @@ app.use(express.static(__dirname + '/public'));
 app.post('/result', (req, res) => {
   const cityName = req.body.city;
   getDataFromApi(res, cityName);
+});
+
+app.post('/multi-results', (req, res) => {
+  let form = new formidable.IncomingForm();
+  form
+    .parse(req)
+    .on('fileBegin', (name, file) => {
+      deleteUploads();
+      file.path = __dirname + '/uploads/' + file.name;
+    })
+    .on('file', (name, file) => {
+      let dataPackage;
+      fs.readFile(`./uploads/${file.name}`, 'utf8', function(error, data) {
+        if (error) {
+          console.log('Whoops! File not found!');
+        }
+        try {
+          dataPackage = JSON.parse(data);
+        } catch (e) {
+          console.log(e.message);
+          return res.json({ Error: 'Uploaded file must be in JSON format' });
+        }
+        const results = (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(results);
+            res.json({ 'Here is the list of all cities': results });
+          }
+        };
+        async.map(dataPackage, getDataForBulkInputs, results);
+      });
+    })
+    .on('aborted', () => {
+      console.error('Request aborted by the user');
+    })
+    .on('error', err => {
+      console.error('Error', err);
+      throw err;
+    });
 });
 
 module.exports = app;
